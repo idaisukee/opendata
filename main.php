0d34e62f38f238f0f57b95617a0b60dce0e5836b
Daisuke Inoue <hibariyashiki@gmail.com>
2016-09-26 22:15:19 +0900

    trimPaths.

--- main.php
+++ main.php
@@ -6,7 +6,6 @@ use GuzzleHttp\Client;
 use Underscore\Types\Arrays as _;
 
 
-
 class Util
 {
 	public function direct_product($array1, $array2)
@@ -178,6 +177,14 @@ class Main
 		return $out;
 		// return $courses;
 	}
+
+
+
+	public function trimPaths(array $paths)
+	{
+		$out = array_map('self::trimPath', $paths);
+		return $out;
+	}
 }
 //echo Main::stationCand('35.6783055555556,139.770441666667,00');h
 $koyamachi = '34.972937,138.384326,tokyo,300';
@@ -190,20 +197,22 @@ $time = '0830';
 //$a = [1, 2, 3];
 //$b = [8, 9, 0];
 
-$o = '23636';
-$d = '887544';
-$p = Main::path($via_list, $date, $time);
-$ob = json_decode($p, false);
+// $o = '23636';
+// $d = '887544';
+// $p = Main::path($via_list, $date, $time);
+// $ob = json_decode($p, false);
 //print_r($ob);
-$tp = Main::trimPath($ob);
-print_r($tp);
+// $tp = Main::trimPath($ob);
+// print_r($tp);
 $a = [23634, 887673];
 $b = [888033, 887544];
 
 //$c = Util::direct_product($a, $b);
 //print_r($c);
-//$e = Main::paths($a, $b, $date, $time);
-//print_r($e);
+$e = Main::paths($a, $b, $date, $time);
+// print_r($e);
+$f = Main::trimPaths($e);
+print_r($f);
 //echo $p;
 //$a = json_decode($p, false);
 //print_r( $a);
