d6017d920b25ca877539e6255901e29f759ca80c
Daisuke Inoue <hibariyashiki@gmail.com>
2016-09-27 20:11:52 +0900

    testcodes.

--- main.php
+++ main.php
@@ -230,35 +230,3 @@ class Main
 		return $geo_point;
 	}
 }
-//echo Main::stationCand('35.6783055555556,139.770441666667,00');
-$koyamachi = '34.972937,138.384326,tokyo,300';
-
-$via_list = '887668:887754';
-$date = '20160603';
-$time = '0830';
-
-
-//$a = [1, 2, 3];
-//$b = [8, 9, 0];
-
-// $o = '23636';
-// $d = '887544';
-// $p = Main::path($via_list, $date, $time);
-// $ob = json_decode($p, false);
-//print_r($ob);
-// $tp = Main::trimPath($ob);
-// print_r($tp);
-$a = [23634, 887673];
-$b = [888033, 887544];
-
-//$c = Util::direct_product($a, $b);
-//print_r($c);
-$e = Main::paths($a, $b, $date, $time);
-// print_r($e);
-$f = Main::trimPaths($e);
-//print_r($f);
-//echo $p;
-//$a = json_decode($p, false);
-//print_r( $a);
-//echo json_decode(Main::stationCand($koyamachi), false);
-//print_r(Main::stationCandList($koyamachi));
