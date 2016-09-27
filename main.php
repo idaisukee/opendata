9255d8b4108cb94bc1b281b8c00cc2bfafa78b7f
Daisuke Inoue <hibariyashiki@gmail.com>
2016-09-27 20:11:40 +0900

    mod proc

--- main.php
+++ main.php
@@ -26,11 +26,23 @@ class Util
 class Main
 {
 
-	public function proc($lat, $long, $radius)
+	public function proc($start_lat, $start_long, $end_lat, $end_long, $radius, $date, $time)
 	{
-		$geo_point = self::geoPoint($lat, $long, $radius);
-		$station_cand = self::stationCand($geo_point);
-		return $station_cand;
+		$start_geo_point = self::geoPoint($start_lat, $start_long, $radius);
+		$start_station_cand = self::stationCand($start_geo_point);
+		$start_trimmed = self::trimStationCand($start_station_cand);
+		$start_list = self::stationCandList($start_geo_point);
+
+		$end_geo_point = self::geoPoint($end_lat, $end_long, $radius);
+		$end_station_cand = self::stationCand($end_geo_point);
+		$end_trimmed = self::trimStationCand($end_station_cand);
+		$end_list = self::stationCandList($end_geo_point);
+
+		$paths = self::paths($start_list, $end_list, $date, $time);
+
+		$trimmed = self::trimPaths($paths);
+		return $trimmed;
+		// return $paths;
 	}
 
 
--- test/testProc.php
+++ test/testProc.php
@@ -1,9 +1,17 @@
 <?php
 
 require '../main.php';
-$lat = '34.972937';
-$long = '138.384326';
-$radius = '300';
-$m = Main::proc($lat, $long, $radius);
+$start_lat = '34.972937';
+$start_long = '138.384326';
+
+$end_lat = '34.972937';
+$end_long = '138.394326';
+
+$radius = '200';
+
+$date = '20160603';
+$time = '0830';
+
+$m = Main::proc($start_lat, $start_long, $end_lat, $end_long, $radius, $date, $time);
 
 print_r($m);
