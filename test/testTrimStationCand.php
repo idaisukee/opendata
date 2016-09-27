5395fd7b5666c0130df9f918eb6fa5f9811f05da
Daisuke Inoue <hibariyashiki@gmail.com>
2016-09-27 22:30:10 +0900

    testcodes.

--- test/testTrimStationCand.php
+++ test/testTrimStationCand.php
@@ -5,4 +5,9 @@ $geo_point = '35.6783055555556,139.770441666667,tokyo,100';
 
 $c = Main::stationCand($geo_point);
 $t = Main::trimStationCand($c);
+// print_r($t);
+
+$geo_point = '34.972937,138.364326,wgs84,300';
+$c = Main::stationCand($geo_point);
+$t = Main::trimStationCand($c);
 print_r($t);
