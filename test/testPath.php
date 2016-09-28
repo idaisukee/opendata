ced4799d982a509c4f1368ae7c1b790768e2a51a
Daisuke Inoue <hibariyashiki@gmail.com>
2016-09-28 10:22:08 +0900

    testcodes.

--- test/testPath.php
+++ test/testPath.php
@@ -3,5 +3,7 @@
 require '../main.php';
 $date = '20160603';
 $time = '0830';
-$via_list = '887668:887754';
+$via_list = '887668:887759';
 $p = Main::path($via_list, $date, $time);
+$o = json_decode($p, false);
+print_r($o);
