a58d08baf414174ddfe77525cbf67691c0bbc438
Daisuke Inoue <hibariyashiki@gmail.com>
2016-09-28 11:56:07 +0900

    testcodes.

--- test/testStationCand.php
+++ test/testStationCand.php
@@ -1,7 +1,7 @@
 <?php
 
 require '../main.php';
-$geo_point = '35.6783055555556,139.770441666667,tokyo,100';
+$geo_point = '35.6783055555556,139.760441666667,tokyo,100';
 $c = Main::stationCand($geo_point);
 $o = json_decode($c, false);
 print_r($o);
