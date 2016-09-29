471fcd7efd0436a0435cc3b8e73fe6b26c272472
Daisuke Inoue <hibariyashiki@gmail.com>
2016-09-29 22:48:51 +0900

    retry.

--- main.php
+++ main.php
@@ -142,8 +142,8 @@ class Main
 			echo $e->getMessage();
 		} catch (HttpException $e) {
 			echo $e->getMessage();
+			return self::retry($start_lat, $start_long, $end_lat, $end_long, $radius, $date, $time);
 		}
-		// return $paths;
 	}
 
 
